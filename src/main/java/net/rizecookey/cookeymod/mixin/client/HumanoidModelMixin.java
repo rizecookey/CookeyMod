383bef35fc73d1b24037543ccfeee3356cf92bdd
Jonas Mittnacht <rizecookey@gmail.com>
2022-01-15 22:05:10 +0100

    Small change

--- src/main/java/net/rizecookey/cookeymod/mixin/client/HumanoidModelMixin.java
+++ src/main/java/net/rizecookey/cookeymod/mixin/client/HumanoidModelMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.world.item.UseAnim;
 import net.rizecookey.cookeymod.CookeyMod;
 import net.rizecookey.cookeymod.config.category.AnimationsCategory;
 import net.rizecookey.cookeymod.config.option.Option;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,10 +23,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HumanoidModel.class)
 public abstract class HumanoidModelMixin<T extends LivingEntity> extends AgeableListModel<T> implements ArmedModel, HeadedModel {
-    @Shadow
+    @Final @Shadow
     public ModelPart rightArm;
 
-    @Shadow
+    @Final @Shadow
     public ModelPart leftArm;
 
     Option<Boolean> showEatingInThirdPerson = CookeyMod.getInstance().getConfig().getCategory(AnimationsCategory.class).showEatingInThirdPerson;
