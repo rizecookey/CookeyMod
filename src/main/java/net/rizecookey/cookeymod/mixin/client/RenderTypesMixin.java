3f9e222b5d904649ea94ac19e884f72027ec8f83
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2026-05-02 21:22:07 +0200

    Change strategy for pipeline and render types modification to prevent breakage with Iris (#20)

--- src/main/java/net/rizecookey/cookeymod/mixin/client/RenderTypesMixin.java
+++ src/main/java/net/rizecookey/cookeymod/mixin/client/RenderTypesMixin.java
@@ -1,83 +1,54 @@
 package net.rizecookey.cookeymod.mixin.client;
 
-import net.minecraft.client.renderer.rendertype.LayeringTransform;
-import net.minecraft.client.renderer.rendertype.OutputTarget;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.renderer.rendertype.RenderSetup;
 import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.resources.Identifier;
-import net.minecraft.util.Util;
-import net.rizecookey.cookeymod.extension.minecraft.RenderPipelinesExtension;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.Function;
 
 @Mixin(RenderTypes.class)
 public abstract class RenderTypesMixin {
-
     @Unique
-    private static final Function<Identifier, RenderType> ARMOR_CUTOUT_NO_CULL_OVERLAY = Util.memoize(
-            texture -> {
-                RenderSetup state = RenderSetup.builder(RenderPipelinesExtension.ARMOR_CUTOUT_NO_CULL_OVERLAY)
-                        .withTexture("Sampler0", texture)
-                        .useLightmap()
-                        .useOverlay()
-                        .setLayeringTransform(LayeringTransform.VIEW_OFFSET_Z_LAYERING)
-                        .affectsCrumbling()
-                        .setOutline(RenderSetup.OutlineProperty.AFFECTS_OUTLINE)
-                        .createRenderSetup();
-                return RenderTypeAccessor.invokeCreate("armor_cutout_no_cull_overlay", state);
-            });
+    private static ScopedValue<Void> ADD_USE_OVERLAY;
 
-    @Unique
-    private static final Function<Identifier, RenderType> ITEM_CUTOUT_OVERLAY = Util.memoize(
-            texture -> {
-                RenderSetup state = RenderSetup.builder(RenderPipelinesExtension.ITEM_CUTOUT_OVERLAY)
-                        .withTexture("Sampler0", texture)
-                        .useLightmap()
-                        .useOverlay()
-                        .affectsCrumbling()
-                        .setOutline(RenderSetup.OutlineProperty.AFFECTS_OUTLINE)
-                        .createRenderSetup();
-                return RenderTypeAccessor.invokeCreate("item_cutout_overlay", state);
-            }
-    );
-
-    @Unique
-    private static final Function<Identifier, RenderType> ITEM_TRANSLUCENT_OVERLAY = Util.memoize(
-            texture -> {
-                RenderSetup state = RenderSetup.builder(RenderPipelinesExtension.ITEM_TRANSLUCENT_OVERLAY)
-                        .withTexture("Sampler0", texture)
-                        .setOutputTarget(OutputTarget.ITEM_ENTITY_TARGET)
-                        .useLightmap()
-                        .useOverlay()
-                        .affectsCrumbling()
-                        .sortOnUpload()
-                        .setOutline(RenderSetup.OutlineProperty.AFFECTS_OUTLINE)
-                        .createRenderSetup();
-                return RenderTypeAccessor.invokeCreate("item_translucent_overlay", state);
-            }
-    );
-
-    @Inject(method = "armorCutoutNoCull", at = @At("HEAD"), cancellable = true)
-    private static void useOverlayVariant(Identifier texture, CallbackInfoReturnable<RenderType> cir) {
-        cir.setReturnValue(ARMOR_CUTOUT_NO_CULL_OVERLAY.apply(texture));
-        cir.cancel();
+    @Inject(method = "<clinit>", at = @At("HEAD"))
+    private static void initializeScopedValue(CallbackInfo ci) {
+        ADD_USE_OVERLAY = ScopedValue.newInstance();
     }
 
-    @Inject(method = "itemCutout", at = @At("HEAD"), cancellable = true)
-    private static void useEntityVariantForCutout(Identifier texture, CallbackInfoReturnable<RenderType> cir) {
-        cir.setReturnValue(ITEM_CUTOUT_OVERLAY.apply(texture));
-        cir.cancel();
+    @WrapOperation(method = "<clinit>", slice = @Slice(
+            from = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/rendertype/RenderTypes;ITEM_CUTOUT:Ljava/util/function/Function;", opcode = Opcodes.PUTSTATIC, shift = At.Shift.BY, by = -1),
+            to = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/rendertype/RenderTypes;ITEM_CUTOUT:Ljava/util/function/Function;", opcode = Opcodes.PUTSTATIC)
+    ), at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util;memoize(Ljava/util/function/Function;)Ljava/util/function/Function;"))
+    private static Function<Identifier, RenderType> markUseOverlayItemCutout(Function<Identifier, RenderType> function, Operation<Function<Identifier, RenderType>> original) {
+        return identifier -> ScopedValue.where(ADD_USE_OVERLAY, null).call(() -> original.call(function).apply(identifier));
     }
 
-    @Inject(method = "itemTranslucent", at = @At("HEAD"), cancellable = true)
-    private static void useEntityVariantForTranslucent(Identifier texture, CallbackInfoReturnable<RenderType> cir) {
-        cir.setReturnValue(ITEM_TRANSLUCENT_OVERLAY.apply(texture));
-        cir.cancel();
+    @WrapOperation(method = "<clinit>", slice = @Slice(
+            from = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/rendertype/RenderTypes;ITEM_TRANSLUCENT:Ljava/util/function/Function;", opcode = Opcodes.PUTSTATIC, shift = At.Shift.BY, by = -1),
+            to = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/rendertype/RenderTypes;ITEM_TRANSLUCENT:Ljava/util/function/Function;", opcode = Opcodes.PUTSTATIC)
+    ), at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util;memoize(Ljava/util/function/Function;)Ljava/util/function/Function;"))
+    private static Function<Identifier, RenderType> markUseOverlayItemTranslucent(Function<Identifier, RenderType> function, Operation<Function<Identifier, RenderType>> original) {
+        return identifier -> ScopedValue.where(ADD_USE_OVERLAY, null).call(() -> original.call(function).apply(identifier));
+    }
+
+    @ModifyExpressionValue(method = "*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/rendertype/RenderSetup;builder(Lcom/mojang/blaze3d/pipeline/RenderPipeline;)Lnet/minecraft/client/renderer/rendertype/RenderSetup$RenderSetupBuilder;"))
+    private static RenderSetup.RenderSetupBuilder addUseOverlay(RenderSetup.RenderSetupBuilder original) {
+        if (ADD_USE_OVERLAY.isBound()) {
+            return original.useOverlay();
+        }
+
+        return original;
     }
 }
